8781cac625d11155e7acfefb9eb3b67c1da6f796
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2025-04-23 21:18:32 +0700

    main: add template

---
+++ resources/views/partials/script.blade.php
@@ -0,0 +1,6 @@
+<script src="{{asset('template/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('template/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset('template/js/ruang-admin.min.js')}}"></script>
+<script src="{{asset('template/vendor/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('template/js/demo/chart-area-demo.js')}}"></script>
